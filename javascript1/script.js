771ea3ff7183d48a1a15057a7c1ebc02b176cb30
Cəmilə Zeynallı <136157631+Jamila27-Zeynal@users.noreply.github.com>
2024-03-04 18:13:37 +0400

    Add files via upload

---
+++ javascript1/script.js
@@ -0,0 +1,187 @@
+ //Vote task.deyisen yaradin ve userden yasini aln deyer olraq.Yasa gore sert qurun.18 den yuxari 70 den asagi ses vere biler.
+ //70 den yuxari ucun hesablayin,ve konsolo yazdirin ki ses ses vermesi ucun nece il artiq zamani var.mes:80-70=10  kecib ses vermesi ucun.eyni sey 18 den asagi olanlara da aiddir
+ 
+ //let age=+prompt("Yasinizi daxil edin:");
+  //if(age >=18 && age < 70){
+   // console.log("ses vere bilersiniz");
+ // } else if(age >= 70){
+   // let il=age -70;
+   // console.log("Ses vermek ucun" + il + "il artiq zamaniniz var");
+  //}else{
+   // let il=18 -age;
+   // console.log("Ses vermek ucun" + il + "il zamaniniz var");
+  //}
+
+
+
+  //task: let road=prompt ("3 yoldan birini secin:1,2,3"),Userden reqem alin eeger eded 3 e tam bolunurse reqem yerie fizz,5 e tam bolunurse buzz,her ikisine bolunurse fizzbuzz
+
+  //let road=+prompt("3 yoldanbirini secin:1,2,3");
+  //let number=+prompt("Bir reqem daxil edin");
+  //if (number % 3 === 0 && number % 5=== 0){
+   //console.log("FizzBuzz");
+  //}else if(number % 3 ===0){
+   // console.log("Fizz");
+ // }
+ // else if(number % 5 ===0){
+   // console.log("Buzz");
+  //}
+  //else{
+   // console.log(number);
+  //}
+
+
+
+
+
+
+//task:const meals=('1.menu first:120$  2.menu second:250$   3.menu basics:350$')
+
+  //const menu=+prompt('1.Menu first :120$  2.Menu second:250$  3.Menu basics:350$');
+ // let tip;
+  //if(menu >= 50 && menu <=300){
+   // tip=0.1;
+ // }
+  //else{
+  //  tip=0.15;
+ // }
+ // const amount=menu*tip;
+  //console.log("hesablanan tip: " + amount*2 + "$");
+
+
+
+//task 5 selsi franheyite ceviren proqram:
+//let selsi=+prompt("Selsi daxil edin");
+//let frangeyt=(selsi*18000)+32;
+//console.log(selsi + " selsi" + frangeyt + "Frangeyte beraberdir");
+
+
+
+
+
+//task 6 aylar:
+//const number=+prompt("1-12 arasi reqem daxil edin");
+//if(number === 1){
+ // console.log("Yanvar");
+//}
+//else if(number === 2){
+ // console.log("Fevral");
+//}
+//else if(number === 3){
+ // console.log("Mart");
+//}
+//else if(number === 4){
+  //console.log("Aprel");
+//}
+//else if(number === 5){
+ // console.log("May");
+//}
+//else if(number === 6){
+ // console.log("Iyun");
+//}
+//else if(number === 7){
+ // console.log("Iyul");
+//}
+//else if(number === 8){
+ // console.log("Avqust");
+//}
+//else if(number === 9){
+ // console.log("Sentyabr");
+//}
+//else if(number === 10){
+//  console.log("Oktyabr");
+//}
+//else if(number === 11){
+ // console.log("Noyabr");
+//}
+//else if(number === 12){
+ // console.log("Dekabr");
+//}
+//else{
+ // console.log("Zehmet olmasa duzgun eded daxil edin.Tesekkurler!");
+//}
+
+
+
+//task 7 musbet,menfi veyaxud 0 olan proqram
+//let number=+prompt("Reqem daxil edin.");
+
+//if(number>0){
+ // console.log("daxil edilen reqem musbet ededdir");
+//}
+//else if(number<0){
+ // console.log("daxil edilmis reqem menfi ededdir");
+//}
+//else{
+  //console.log("Daxil edilen reqem 0-dir");
+//}
+
+
+
+//task 8
+ // let uzunluq=+prompt("Duzbucaqlinin uzunlugunu qeyd edin");
+ // let eni=+prompt("Duzbucaqlinin enini daxil edin");
+  
+ // if(uzunluq=== 0|| eni === 0){
+ // console.log("uzunluq sifir deyil");
+ 
+//}
+
+//else{
+ // let perimetr=2*(uzunluq+eni);
+//console.log("duzbucaqlinin sahesi:" +perimetr);
+//}
+
+
+
+//FOR
+
+//task 9
+//let string="string";
+//let tersstring ="";
+//for (let i =string.length - 1; i >= 0; i--){
+ // tersstring += string[i];
+//}
+//console.log(tersstring);
+
+
+
+//For
+//task 10
+//const array=[2,3,6,8,9];
+//let cem=0;
+//for(let i=0; i<array.length; i++){
+ // cem += array[i];
+//}
+//console.log("Arrayin cemi:" +cem);  
+
+
+//task 11 vurma cedveli
+//for(let i=1; i <=10; i++){
+ //for(let j =1; j<=10; j++){
+   // console.log('${i} * ${j} = ${i*j}' );
+  //}
+ // console.log("----------");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
